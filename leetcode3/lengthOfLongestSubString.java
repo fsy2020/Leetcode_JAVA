744a7a38a22d51f7f31c97f01ba0b6073e3878d3
fsy2020 <fsy781653822@gmail.com>
2022-04-11 10:58:59 +0800

    2022-4-11 暴力解法

--- leetcode3/lengthOfLongestSubString.java
+++ leetcode3/lengthOfLongestSubString.java
@@ -1,30 +1,47 @@
 package leetcode3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class lengthOfLongestSubString {
 
     public static void main(String[] args) {
         String s = "pwwkew";
-        int i = lengthOfLongestSubstring(s);
+        int i = lengthOfLongestSubstring2(s);
         System.out.println(i);
     }
 
     public static int lengthOfLongestSubstring(String s){
-        int left =0;
-        int right = 1;
-        if(s.charAt(left)==s.charAt(right )&& right==left+1){
-            left++;
-            right++;
-        }else if(s.charAt(left)==s.charAt(right) && right!=left+1){
-            left++;
-
-        }else{
-            while(s.charAt(left)!=s.charAt(right)){
-                right++;
-            }
+        int sum =0;
+        char[] c =s.toCharArray();
+        for(int i=0;i<c.length;i++){
+            Set<Character> set = new HashSet<>();
+            int maxlen = 0;
+            int j = i;
+            while(j<c.length && !set.contains(c[j])) {
+                    set.add(c[j]);
+                    maxlen++;
+                    j++;
+                }
+            sum = Math.max(maxlen,sum);
         }
-
-
-        return right-left;
-
+        return sum;
     }
+
+    public static int lengthOfLongestSubstring2(String s){
+        int res = 0;
+        int left = 0;
+        char[] c = s.toCharArray();
+        HashMap<Character,Integer> map =new HashMap<>();
+        for(int i = 0;i<c.length;i++){
+            if(map.containsKey(c[i])){
+                left = Math.max(left,map.get(c[i])+1);
+            }
+            map.put(c[i],i);
+            res = Math.max(res,i-left+1);
+        }
+        return res;
+    }
+
 }
