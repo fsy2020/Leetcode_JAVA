f479c4112ca0cafedef31f8f51d51e1a5ae26309
fsy2020 <fsy781653822@gmail.com>
2022-04-17 10:47:55 +0800

    2022-4-17 海康威视

---
+++ cp6070/digitSum.java
@@ -0,0 +1,17 @@
+package cp6070;
+
+public class digitSum {
+
+    //十分钟没思路，应该是用递归来做。
+    public static void main(String[] args) {
+
+    }
+
+    public String digitSum(String s, int k) {
+        char[] c = s.toCharArray();
+        for(int i=0;i<c.length;i++){
+            int digit = c[i] - '0';
+
+        }
+    }
+}
