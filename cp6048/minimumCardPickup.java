f47303ec80ff1cf7de72ff520e2ec1716b6a9a99
fsy2020 <fsy781653822@gmail.com>
2022-05-02 15:43:10 +0800

    2022/5/2  用HashMap解决，竞赛时没有想到。

--- cp6048/minimumCardPickup.java
+++ cp6048/minimumCardPickup.java
@@ -1,4 +1,7 @@
 package cp6048;
+
+import java.util.HashMap;
+
 /*
     想法不对，扫描过一遍就是暴力解法。放在while循环里并没有优化。
 
@@ -7,7 +10,7 @@ public class minimumCardPickup {
     public static void main(String[] args) {
         int[] cards = new int[]{95,11,8,65,5,86,30,27,30};
 
-        int i = minimumCardPickup(cards);
+        int i = minimumCardPickup2(cards);
         System.out.println(i);
     }
     public static int minimumCardPickup(int[] cards) {
@@ -23,5 +26,18 @@ public class minimumCardPickup {
             }
         }
         return min == Integer.MAX_VALUE ? -1: min;
+    }
+
+    public static int minimumCardPickup2(int[] cards) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        for(int i = 0;i<cards.length;i++){
+            if(map.containsKey(cards[i])){
+                min = Math.min(min,i-map.get(cards[i])+1);
+            }
+            map.put(cards[i],i);
+        }
+        return min;
+
     }
 }
