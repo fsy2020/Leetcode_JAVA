aa8a00be9a67cfdbfbff4f9c1160d33f3191615e
fsy2020 <fsy781653822@gmail.com>
2022-05-03 11:54:20 +0800

    2022/5/3  compareTo(),charAt()方法

--- cp6047/removeDigit.java
+++ cp6047/removeDigit.java
@@ -1,9 +1,14 @@
 package cp6047;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class removeDigit {
     public static void main(String[] args) {
-        String s = removeDigit("1231", '1');
-        System.out.println(s);
+        String s = removeDigit2("1231", '1');
+        String s2 = s.substring(0, 1) + s.substring(2, 4);
+        System.out.println(s2);
 
     }
     public static String removeDigit(String number, char digit) {
@@ -17,6 +22,20 @@ public class removeDigit {
             }
         }
         return sb.reverse().toString();
+    }
+
+    public static String removeDigit2(String number, char digit) {
+        int n = number.length();
+        List<String> list = new ArrayList<>();
+        char []c = number.toCharArray();
+        for(int i = 0;i<c.length;i++) {
+            if (c[i] == digit) {
+                String s = number.substring(0, i) + number.substring(i + 1, n);
+                list.add(s);
+            }
+        }
+        Collections.sort(list);
+        return list.get(list.size()-1);
     }
 }
 
